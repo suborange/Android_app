85da80cc953b20a6e57737258815bfcc72aa2043
suborange <ebonavida0@yahoo.com>
2023-04-17 02:56:13 -0700

    add current journey activity_current_journey.xml; setup skeleton for activity layout

---
+++ Android_project_02/GymJourney/app/src/main/java/com/example/project_02/GymJourney/CurrentJourneyActivity.java
@@ -0,0 +1,20 @@
+package com.example.project_02.GymJourney;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CurrentJourneyActivity extends AppCompatActivity {
+
+
+    /**
+     * TODO make select workout a click event, and go into the workout by long clicking.
+     * or just make two buttons, to add one or delete one by name?
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_journey);
+    }
+}
